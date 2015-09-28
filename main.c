a3ed570173a7d101f67d38a378c78215a939d22f
echoline <echoline@localhost>
2015-09-28 14:56:15 -0700

    moar

--- main.c
+++ main.c
@@ -50,6 +50,9 @@ parse (flap *f){
 		write (1, "welcome, ", 9);
 		write (1, &f->data[f->offset], len);
 		write (1, "\n", 2);
+#ifdef __linux
+		fflush(stdout);
+#endif
 		break;
 
 	case 0x00010005:
@@ -90,6 +93,42 @@ parse (flap *f){
 
 	case 0x00010021:
 		break;
+
+	case 0x000b0002:
+		print ("stats interval: %d\n", get2(f));
+		break;
+
+	case 0x0003000b:
+	case 0x0003000c:
+		print ("0x%04x 0x%04x\n", rs.family, rs.subtype);
+		do {
+			if (rs.subtype == 0x000c)
+				print ("offline: ");
+			else if (rs.subtype == 0x000b)
+				print ("online: ");
+#ifdef __linux
+			fflush(stdout);
+#endif
+			write (1, &f->data[f->offset + 1], f->data[f->offset]);
+#ifdef __linux
+			fflush(stdout);
+#endif
+			f->offset += f->data[f->offset] + 1;
+			s = get2(f);
+			print(", warn: 0x%02x\n", s);
+			s = get2(f);
+
+			for (i = 0; i < s; i++) {
+				t = recvtlv(f);
+				print("t: 0x%04x, l: %d\n", t->type, t->length);
+				freetlv(t);
+			}
+		} while (f->offset < f->length);
+		break;
+
+//	case 0x00040007:
+//		print ("0x%04x 0x%04x\n", rs.family, rs.subtype);
+//		break;
 	}
 }
 
